32da3a69b2fa9ede54d3434b47e885ff1ceb8883
venugopal0106 <133962344+venugopal0106@users.noreply.github.com>
2024-02-15 22:11:39 +0530

    Add files via upload

---
+++ CollectionMap.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+import java.util.*;
+
+public class CollectionMap{
+    public static void main(String[] args) {
+        Map<String,Integer> det =new HashMap<>();
+        det.put("Venu",12 );
+        det.put("Gopal",18 );
+        det.put("VG",24 );
+        System.out.println(det);
+
+    }
+} 
