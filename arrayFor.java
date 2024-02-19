3ff0798eb33498be7c97212ce70e3fcf7099d829
venugopal0106 <133962344+venugopal0106@users.noreply.github.com>
2024-02-19 20:36:15 +0530

    Add files via upload

---
+++ arrayFor.java
@@ -0,0 +1,10 @@
+public class arrayFor {
+    public static void main(String[] args){
+        String[] names ={"vg","vicky","sunny","money"};
+        for( String i: names){
+            // if (i=="sunny"){
+            System.out.print(i+" ");
+            // }
+        }
+    } 
+}
