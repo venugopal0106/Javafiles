1c97e6111fb14fb5119e45cc21d34d5680a6e2e5
venugopal0106 <133962344+venugopal0106@users.noreply.github.com>
2024-02-20 23:49:09 +0530

    Add files via upload

---
+++ MathTables.java
@@ -0,0 +1,16 @@
+public class MathTables {
+    public static void table(int num){
+        int i;
+        int ans;
+        for(i=1;i<=10;i++){
+            ans=num*i;
+            System.out.println(num+"*"+i+"="+ans);
+        }
+
+    } 
+public static void main(String[] args) {
+    tesing1 obj =new tesing1();
+    obj.table(234);
+
+    }
+}
