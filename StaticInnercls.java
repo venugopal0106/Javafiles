f78f38bb54099e17722766d8e474845fcf52eeb4
venugopal0106 <133962344+venugopal0106@users.noreply.github.com>
2024-02-25 21:56:11 +0530

    Add files via upload

--- StaticInnercls.java
+++ StaticInnercls.java
@@ -4,10 +4,20 @@ class outer{
             System.out.println("From static inner class");
         }
     }
+public class A{
+    public void M1(){
+        System.out.println("From normal class");
+    }
+}
 }
 public class StaticInnercls {
     public static void main(String[] args) {
     outer.inner.M();
+    // so,here without creating the instance of the we are calling the 
+    // method that means we no need to crate an object for calling a static method
+    outer obj1 =new outer();
+    outer.A  obj2 =obj1.new A();
+    obj2.M1();
         
     }    
 }
