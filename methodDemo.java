2c9a3e7764445e50103527d11712bdc455b1fc07
venugopal0106 <133962344+venugopal0106@users.noreply.github.com>
2024-03-11 23:44:30 +0530

    Add files via upload

---
+++ methodDemo.java
@@ -0,0 +1,12 @@
+class methodDemo{
+    int x;
+}
+static void method(){
+    methodDemo s1 = new methodDemo();
+    s1.x=34;
+    System.out.println(s1.x);
+}
+    public static void main(String[] args) {
+        method();
+    }    
+
