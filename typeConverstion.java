d3d5cf9eac8d1c80956e477482778149e0c7eae0
venugopal0106 <133962344+venugopal0106@users.noreply.github.com>
2024-03-24 23:47:04 +0530

    Add files via upload

---
+++ typeConverstion.java
@@ -0,0 +1,9 @@
+public class typeConverstion {
+    public static void main(String[] args)
+{
+    int a=12;
+    float b =13.11f;
+    int c = (int)((a*a)+(b*b)+(2*a*b));
+    System.out.println(c);
+}
+}
